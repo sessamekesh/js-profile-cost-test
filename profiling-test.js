ca5e12dba54a8a7c272e57eb36f961c7122f3715
Kamaron Peterson <kamaron@lucidchart.com>
2016-08-22 17:54:10 -0600

    Refactored profile test to make a little easier to write

--- profiling-test.js
+++ profiling-test.js
@@ -14,39 +14,78 @@ sess.demo.Profile = sess.demo.Profile || {};
  *  (4): Capture all low-level functions (the sort)
  *  (5): Capture all functions together
  */
-sess.demo.Profile.Run = function (frameCountPerSample, scene) {
-	sess.demo.Profile.RunFrameOnlyTests(frameCountPerSample, scene);
+sess.demo.Profile.Run = function (maxTimeElapsed, maxBufferSize, scene) {
+	sess.demo.Profile.RunFrameOnlyTests(maxTimeElapsed, maxBufferSize, scene);
 };
 
-sess.demo.Profile.RunFrameOnlyTests = function (frameCountPerSample, scene) {
+// Invoke cb when exit false condition hit
+sess.demo.Profile.WrapMethod = function (fn, fnId, sharedSettings, idBuffer, timeBuffer, cb) {
+	var startTime = 0;
+	var endTime = 0;
+
+	// Settings:
+	//  - fnID
+	//  - stopTime
+	//  - nextBufferIdx
+	//  - maxBufferSize
+
+	return {
+		original: fn,
+		method: function () {
+			startTime = performance.now();
+			fn.apply(this, arguments);
+			endTime = performance.now();
+
+			if (endTime < sharedSettings.stopTime && sharedSettings.nextBufferIdx < sharedSettings.maxBufferSize) {
+				idBuffer[sharedSettings.nextBufferIdx] = fnId;
+				timeBuffer[sharedSettings.nextBufferIdx] = endTime - startTime;
+				sharedSettings.nextBufferIdx++;
+			} else {
+				cb();
+			}
+		}
+	};
+};
+
+sess.demo.Profile.RunFrameOnlyTests = function (maxTimeElapsed, maxBufferSize, scene) {
 	async.mapSeries([
 		200, 2000, 20000
 	], function (val, cb) {
+		var stopTestTime = performance.now() + maxTimeElapsed;
+		var nextBufferIndex = 0;
+		var FN_FRAME_ID = 0;
 		scene.setNumParticles(val);
-		var frameTimeBuffer = new Float32Array(frameCountPerSample);
-		var frameIndex = 0;
-		var oldFrameMethod = sess.demo.Scene.prototype.frame;
-		var invokeCallback = true;
-		var startTime = 0;
-		var endTime = 0;
-		sess.demo.Scene.prototype.frame = function (pn) {
-			startTime = performance.now();
-			oldFrameMethod.call(this, pn);
-			endTime = performance.now();
+		var methodCalledBuffer = new Uint8Array(maxBufferSize);
+		var frameTimeBuffer = new Float32Array(maxBufferSize);
+		var alreadyFinished = false;
 
-			if (frameIndex < frameCountPerSample) {
-				frameTimeBuffer[frameIndex] = endTime - startTime;
-				frameIndex++;
-			} else if (invokeCallback) {
-				sess.demo.Scene.prototype.frame = oldFrameMethod;
-				var average = 0;
-				for (var i = 0; i < frameCountPerSample; i++) {
-					average += frameTimeBuffer[i] / frameCountPerSample;
-				}
-				invokeCallback && cb(null, average);
-				invokeCallback = false;
-			}
+		var sharedSettings = {
+			nextBufferIdx: 0,
+			stopTime: stopTestTime,
+			maxBufferSize: maxBufferSize
 		};
+
+		var onFinish = function () {
+			if (alreadyFinished) {
+				return;
+			}
+
+			alreadyFinished = true;
+
+			// Restore original methods
+			sess.demo.Scene.prototype.frame = frameWrapped.original;
+
+			// Collect metrics
+			var frameTimeAverage = 0;
+			for (var i = 0; i < sharedSettings.nextBufferIdx; i++) {
+				frameTimeAverage += frameTimeBuffer[i] / sharedSettings.nextBufferIdx;
+			}
+			cb(null, frameTimeAverage);
+		};
+
+		var frameWrapped = sess.demo.Profile.WrapMethod(sess.demo.Scene.prototype.frame, 0, sharedSettings, methodCalledBuffer, frameTimeBuffer, onFinish);
+		sess.demo.Scene.prototype.frame = frameWrapped.method;
+
 	}, function (err, result) {
 		console.log('Frame times (ms) for various number of particles:');
 		console.log('200: ' + result[0] + '  2000: ' + result[1] + '  20000: ' + result[2]);
